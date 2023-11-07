bdeead7cb1ccf276d86387af762e19a95ddc3e27
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-11-07 19:27:00 +0300

    chore: add user nav component

---
+++ components/dashboard/user-nav.tsx
@@ -0,0 +1,89 @@
+'use client'
+
+import { User } from '@prisma/client'
+import { User2 } from 'lucide-react'
+import { signIn, signOut } from 'next-auth/react'
+
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu'
+
+type UserNavProps = {
+    user: Pick<User, 'id' | 'email' | 'image' | 'name'> | undefined
+}
+
+const UserNav: React.FC<UserNavProps> = ({ user }) => {
+    if (!user)
+        return (
+            <Button
+                variant='ghost'
+                onClick={() =>
+                    signIn('google', { redirect: true, callbackUrl: '/' })
+                }
+            >
+                Sign in
+            </Button>
+        )
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant='ghost'
+                    className='relative h-8 w-8 rounded-full'
+                >
+                    <Avatar className='h-8 w-8'>
+                        <AvatarImage
+                            src={user.image as string}
+                            alt={`${user.name}'s picture`}
+                        />
+                        <AvatarFallback>
+                            <User2
+                                size={32}
+                                className='text-muted-foreground'
+                            />
+                        </AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent
+                className='max-h-72 w-40'
+                align='end'
+                forceMount
+            >
+                <DropdownMenuLabel className='font-normal'>
+                    <div className='flex flex-col space-y-1.5'>
+                        <p className='text-sm font-medium leading-none'>
+                            {user.name}
+                        </p>
+                        <p className='text-xs leading-none text-muted-foreground'>
+                            {user.email}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>Profile</DropdownMenuItem>
+                    <DropdownMenuItem>Billing</DropdownMenuItem>
+                    <DropdownMenuItem>Settings</DropdownMenuItem>
+                    <DropdownMenuSeparator />
+                    <DropdownMenuItem
+                        onClick={() => signOut()}
+                        className='cursor-pointer'
+                    >
+                        Log out
+                    </DropdownMenuItem>
+                </DropdownMenuGroup>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default UserNav
