bd7d7a27d01a6d2cdf14995fd6886c263def379d
Basheer Rajeh <muhammadbasheerrajeh@hotmail.com>
2023-11-07 13:52:47 +0300

    chore: implement authentication with prisma adapter

---
+++ app/api/(routes)/auth/[...nextauth]/auth-options.ts
@@ -0,0 +1,55 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import bcrypt from 'bcryptjs'
+import { AuthOptions } from 'next-auth'
+import CredentialsProvider from 'next-auth/providers/credentials'
+import GoogleProvider from 'next-auth/providers/google'
+
+import db from '@/lib/db'
+
+const invalidMessage = 'Invalid credentials'
+const authOptions = {
+    adapter: PrismaAdapter(db),
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+        }),
+
+        CredentialsProvider({
+            name: 'credentials',
+            credentials: {
+                email: { label: 'email', type: 'text' },
+                password: { label: 'password', type: 'password' },
+            },
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials.password) {
+                    throw new Error(invalidMessage)
+                }
+
+                const user = await db.user.findUnique({
+                    where: {
+                        email: credentials.email,
+                    },
+                })
+
+                if (!user || !user?.hashedPassword) {
+                    throw new Error(invalidMessage)
+                }
+
+                const isCorrectPassword = await bcrypt.compare(
+                    credentials.password,
+                    user.hashedPassword,
+                )
+
+                if (!isCorrectPassword) throw new Error(invalidMessage)
+
+                return user
+            },
+        }),
+    ],
+    debug: process.env.NODE_ENV === 'development',
+
+    secret: process.env.NEXTAUTH_SECRET,
+} satisfies AuthOptions
+
+export default authOptions
